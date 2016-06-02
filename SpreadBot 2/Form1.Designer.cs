70c82a7f020dd380711384b5e8a0fcc3e4c98708
BuildTools <unconfigured@null.spigotmc.org>
2016-06-02 12:15:24 +0200

    1.0

---
+++ SpreadBot 2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SpreadBot_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonBlue1 = new ButtonBlue();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SpreadBot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(32, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login using Skype credentials.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nickname";
+            // 
+            // buttonBlue1
+            // 
+            this.buttonBlue1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonBlue1.Font = new System.Drawing.Font("Arial", 10F);
+            this.buttonBlue1.Image = null;
+            this.buttonBlue1.Location = new System.Drawing.Point(83, 175);
+            this.buttonBlue1.Name = "buttonBlue1";
+            this.buttonBlue1.NoRounding = false;
+            this.buttonBlue1.Size = new System.Drawing.Size(115, 23);
+            this.buttonBlue1.TabIndex = 6;
+            this.buttonBlue1.Text = "Login";
+            this.buttonBlue1.Click += new System.EventHandler(this.buttonBlue1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(294, 219);
+            this.Controls.Add(this.buttonBlue1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private ButtonBlue buttonBlue1;
+
+
+    }
+}
+
---
+++ SpreadBot 2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Skype4Sharp;
+using Skype4Sharp.Auth;
+using Skype4Sharp.Enums;
+using Skype4Sharp.Events;
+using Skype4Sharp.Exceptions;
+using Skype4Sharp.Helpers;
+
+namespace SpreadBot_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Skype4Sharp.Skype4Sharp mainSkype;
+        private void buttonBlue1_Click(object sender, EventArgs e)
+        {
+            SkypeCredentials authCreds = new SkypeCredentials(textBox1.Text, textBox2.Text);
+            try
+            {
+                mainSkype = new Skype4Sharp.Skype4Sharp(authCreds);
+                mainSkype.Login();
+                mainSkype.StartPoll();
+                MessageBox.Show("Logged in!");
+            } 
+            catch
+            {
+                MessageBox.Show("[01] Failed to login."
+                    + Environment.NewLine + "Make sure that you typed" 
+                    + Environment.NewLine + "correctly name / password."
+                    + Environment.NewLine
+                    + Environment.NewLine + "Troubles? 'federicoretro' on Skype.");
+            }
+        }
+
+        private static void MainSkype_contactRequestReceived(ContactRequest sentRequest, ChatMessage pMessage)
+        {
+            Thread.Sleep(10000);
+            sentRequest.Accept();
+        }
+
+        public static void CreateEmptyFile(string filename)
+        {
+            File.Create(filename).Dispose();
+        }
+
+
+        private static void MainSkype_messageReceived(ChatMessage pMessage, Chat targetChat)
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    pMessage.Chat.Topic = "hey contact me babe! ;)";
+                }
+                catch
+                {
+                    string Log = "data/Log.txt";
+                    string ChatEnded = "SPREADBOT <Chat ended, maybe implement Template2?>";
+                    string[] Template = File.ReadAllLines("data/Template.txt");
+                    string User = "user/" + pMessage.Sender.Username;
+                    string UserLine = File.ReadAllText(User);
+                    if (File.Exists(User))
+                    {
+                        foreach (string item in Template)
+                        {
+
+                            if (UserLine.Contains(item))
+                            {
+                                try
+                                {
+                                    Thread.Sleep(5000);
+                                    pMessage.Chat.SendMessage(User);
+                                    File.AppendAllText(Log, Environment.NewLine + "SpreadBot <TEMPLATELOG> - Sent \"" + User + "\" to '" + pMessage.Sender.Username + "' .");
+                                    File.WriteAllText(User, item + 1);
+                                }
+                                catch
+                                {
+                                    File.AppendAllText(Log, Environment.NewLine + "SpreadBot <TEMPLATELOG> - Got the end of the chat, nothing to say.");
+                                    File.WriteAllText(User, ChatEnded);
+                                    break;
+                                }
+                            }
+                            else if (UserLine.Contains(ChatEnded))
+                            {
+                                File.AppendAllText(Log, Environment.NewLine + "SpreadBot <TEMPLATELOG> - Chat ended, nothing to say.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Thread.Sleep(5000);
+                        CreateEmptyFile(User);
+                        File.AppendAllText(Log, Environment.NewLine + "SpreadBot <TEMPLATELOG> - Started chat with '" + pMessage.Sender.Username + "' .");
+                        File.AppendAllText(User, Template[0]);
+                        pMessage.Chat.SendMessage(User);
+                    }
+                }
+            }).Start();
+        }
+    }
+}
